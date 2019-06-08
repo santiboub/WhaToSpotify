4f94bb65223b31f77635c72d5cdf1af1324eead2
santiboub <santiagobou2@gmail.com>
2019-06-08 19:32:02 +0200

    Add Target

---
+++ src/main/java/com/santibb/whatSpot/App.java
@@ -0,0 +1,13 @@
+package com.santibb.whatSpot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
